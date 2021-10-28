286409bf7d16783a14291c1d1a209a651f5c2ba8
alberto <albertozanola1996@gmail.com>
2021-10-28 08:37:35 +0200

    Inserimento fumetti con foreach

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -3,7 +3,10 @@
 @section('title', 'Comics')  
 
 @section('content')
-    <h3>i miei fumetti</h3>
-    @dump($fumetti)
-    <h1></h1>
+    <h2>i miei fumetti</h2>
+    @foreach ($fumetti as $fumetto)
+        <img src=" {{$fumetto['thumb']}}">
+        {{ $fumetto['series'] }}
+        <br/>
+    @endforeach
 @endsection
