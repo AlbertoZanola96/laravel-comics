70ca63842acce80e25ee07692b19b15f7dce07df
alberto <albertozanola1996@gmail.com>
2021-10-28 09:27:50 +0200

    Inizio struttura header & footer

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,16 +1,20 @@
 <header>
-    <h1>sono l'header</h1>
+    <div id="logo">
+        <img src="{{ asset('images/dc-logo.png')}}" alt="">
+    </div>
 
-    <ul>
-        <li><a href="/characters">CHARACTERS</a></li>
-        <li><a href="/comics">COMICS</a></li>
-        <li><a href="#">MOVIES</a></li>
-        <li><a href="#">TV</a></li>
-        <li><a href="#">GAMES</a></li>
-        <li><a href="#">COLLECTIBLES</a></li>
-        <li><a href="#">VIDEOS</a></li>
-        <li><a href="#">FANS</a></li>
-        <li><a href="#">NEWS</a></li>
-        <li><a href="#">SHOP</a></li>
-    </ul>
+    <nav>
+        <ul>
+            <li><a href="/characters">CHARACTERS</a></li>
+            <li><a href="/comics">COMICS</a></li>
+            <li><a href="#">MOVIES</a></li>
+            <li><a href="#">TV</a></li>
+            <li><a href="#">GAMES</a></li>
+            <li><a href="#">COLLECTIBLES</a></li>
+            <li><a href="#">VIDEOS</a></li>
+            <li><a href="#">FANS</a></li>
+            <li><a href="#">NEWS</a></li>
+            <li><a href="#">SHOP</a></li>
+        </ul>
+    </nav>
 </header>
